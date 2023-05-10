b8517255a0b1eaa67b4c95155ef027cdaa90ff14
NjengaBen <bennjenga2@gmail.com>
2023-05-10 20:38:32 +0300

    body changes

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
+import Head from "next/head";
 
 // `app/page.js` is the UI for the root `/` URL
 const posts = () => {
   return (
-    <div>
-      <Link
-        className="font-bold text-blue-800 hover:text-blue-300"
-        href="posts/{1}"
-      >
-        My first post
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Posts</title>
+      </Head>
+      <div>
+        <Link
+          className="font-bold text-blue-800 hover:text-blue-300"
+          href="posts/{1}"
+        >
+          My first post
+        </Link>
+      </div>
+    </>
   );
 };
 
